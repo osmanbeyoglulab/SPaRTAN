1206916d4facc4149843c7872eddb9b7946b2a3e
april1729 <Tjs2mx85znUNsUA>
2022-08-01 12:33:04 -0400

    added /dist/ to gitignore

--- dist/pySPaRTAN-0.0.3/pySPaRTAN/__init__.py
+++
@@ -1,7 +0,0 @@
-import pySPaRTAN.pl
-import pySPaRTAN.pp
-import pySPaRTAN.datasets
-import cythLeastR
-import cythKronPlus
-from pySPaRTAN.pySPaRTAN import SPaRTAN
-
--- dist/pySPaRTAN-0.0.3/pySPaRTAN/cythLeastR/ep21R.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef _EP21R_H_
-#define _EP21R_H_
-
-void _ep21R(double * x, double *t, double * u, double * v, int n, int k);
-
-#endif // _EP21R_H_
